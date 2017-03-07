3ee9a5b9ccdfe1286acd091b21a530040c9b97dc
Dan Mork <danmork@gmail.com>
2017-03-07 16:50:46 -0600

    Remove Old Func

--- src/views/index.js
+++ src/views/index.js
@@ -2,10 +2,6 @@ import * as building from "./building";
 import * as attaching from "./attaching";
 import * as finishing from "./finishing";
 
-export function setPlatform(platform) {
-  attaching.setPlatform(platform);
-}
-
 export {
   building,
   attaching,
