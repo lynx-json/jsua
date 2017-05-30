ead390d957f8a11e047ef715cb7895fde489824d
John Howes <john.howes@gmail.com>
2017-05-30 13:28:40 -0500

    Fix for Scrolling/Focus Behavior

--- src/finishing.js
+++ src/finishing.js
@@ -1,30 +1,48 @@
 export var registrations = [];
 
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+
+  var body = document.body;
+  var docEl = document.documentElement;
+
+  var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+  var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+  var clientTop = docEl.clientTop || body.clientTop || 0;
+  var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+  var top = box.top + scrollTop - clientTop;
+  var left = box.left + scrollLeft - clientLeft;
+
+  return { top: Math.round(top), left: Math.round(left) };
+}
+
 export function finish(result) {
   if (!result) throw new Error("'result' param is required.");
   if (!result.view) throw new Error("'result' object must have 'view' property.");
   if (!result.content) throw new Error("'result' object must have 'content' property.");
-  
+
   registrations.forEach(registration => {
     if (registration.condition && registration.condition(result) === false) return;
-    
+
     if (typeof registration.finisher === "function") {
       registration.finisher(result);
     } else if (Array.isArray(registration.finisher)) {
       registration.finisher.forEach(finisher => finisher(result));
     }
   });
-  
+
   var focusedViews = Array.from(result.view.querySelectorAll("[data-jsua-focus=true]"));
   if (result.view.matches("[data-jsua-focus=true]")) focusedViews.unshift(result.view);
-  
-  focusedViews.forEach( (focusedView, idx) => {
+
+  focusedViews.forEach((focusedView, idx) => {
     focusedView.removeAttribute("data-jsua-focus");
     if (idx !== 0) return;
-    document.body.scrollTop = focusedView.offsetHeight;
+    document.body.scrollTop = getCoords(focusedView).top;
     focusedView.focus();
   });
-  
+
   return result;
 }
 
@@ -32,11 +50,11 @@ export function register(name, finisher, condition) {
   if (!name) throw new Error("'name' param is required.");
   if (!finisher) throw new Error("'finisher' param is required.");
   if (condition && typeof condition !== "function") throw new Error("'condition' param must be a function.");
-  
+
   condition = condition || null;
   var newRegistration = { name, finisher, condition };
   var oldRegistration = registrations.find(registration => registration.name === name);
-  
+
   if (oldRegistration) {
     let index = registrations.indexOf(oldRegistration);
     registrations[index] = newRegistration;
--- test/finishing.js
+++ test/finishing.js
@@ -6,14 +6,14 @@ var expect = chai.expect;
 var sinon = require("sinon");
 
 function createView() {
-  var view = { 
+  var view = {
     matches: sinon.stub(),
     querySelectorAll: sinon.stub()
   };
-  
+
   view.matches.returns(false);
   view.querySelectorAll.returns([]);
-  
+
   return view;
 }
 
@@ -23,130 +23,134 @@ describe("finishing", function () {
       finishing.finish();
     }).to.throw(Error);
   });
-  
+
   it("should throw when param is not an object", function () {
     expect(function () {
       finishing.finish("this is not a build/attach result");
     }).to.throw(Error);
   });
-  
+
   it("should throw when param doesn't have a 'content' property", function () {
     expect(function () {
       finishing.finish({});
     }).to.throw(Error);
   });
-  
+
   it("should throw when param doesn't have a 'view' property", function () {
     expect(function () {
       finishing.finish({ content: {} });
     }).to.throw(Error);
   });
-  
+
   it("should not throw when param has 'content' and 'view' properties", function () {
     expect(function () {
       finishing.finish({ content: {}, view: createView() });
     }).to.not.throw(Error);
   });
-  
+
   it("should call finisher functions with result", function () {
     let result = { content: {}, view: createView() };
-    
+
     let finA = sinon.spy();
     finishing.register("finA", finA);
-    
+
     let finB = sinon.spy();
     finishing.register("finB", finB);
-    
+
     finishing.finish(result);
-    
+
     finA.calledWith(result).should.be.true;
     finB.calledWith(result).should.be.true;
   });
-  
+
   it("should not call finisher when condition is unmet", function () {
     let result = { content: {}, view: createView() };
-    
+
     let condition = sinon.stub();
     condition.returns(false);
     let finisher = sinon.spy();
-    
+
     finishing.register("finisher", finisher, condition);
     finishing.finish(result);
-    
+
     condition.calledWith(result).should.be.true;
     finisher.calledOnce.should.be.false;
   });
-  
+
   it("should call finisher when condition is met", function () {
     let result = { content: {}, view: createView() };
-    
+
     let condition = sinon.stub();
     condition.returns(true);
     let finisher = sinon.spy();
-    
+
     finishing.register("finisher", finisher, condition);
     finishing.finish(result);
-    
+
     condition.calledWith(result).should.be.true;
     finisher.calledWith(result).should.be.true;
   });
-  
+
   it("should call finisher arrays with result", function () {
     let result = { content: {}, view: createView() };
     let finisher = [];
-    
+
     let finA = sinon.spy();
     finisher.push(finA);
-    
+
     let finB = sinon.spy();
     finisher.push(finB);
-    
+
     finishing.register("finisher-array", finisher);
     finishing.finish(result);
-    
+
     finA.calledWith(result).should.be.true;
     finB.calledWith(result).should.be.true;
   });
-  
+
   it("should not call finisher arrays when condition is unmet", function () {
     let result = { content: {}, view: createView() };
     let finisher = [];
-    
+
     let finA = sinon.spy();
     finisher.push(finA);
-    
+
     let finB = sinon.spy();
     finisher.push(finB);
-    
+
     let condition = sinon.stub();
     condition.returns(false);
-    
+
     finishing.register("finisher-array", finisher, condition);
     finishing.finish(result);
-    
+
     condition.calledWith(result).should.be.true;
     finA.calledOnce.should.be.false;
     finB.calledOnce.should.be.false;
   });
-  
+
   it("should call finisher arrays when condition is met", function () {
     let result = { content: {}, view: createView() };
     let finisher = [];
-    
+
     let finA = sinon.spy();
     finisher.push(finA);
-    
+
     let finB = sinon.spy();
     finisher.push(finB);
-    
+
     let condition = sinon.stub();
     condition.returns(true);
-    
+
     finishing.register("finisher-array", finisher, condition);
     finishing.finish(result);
-    
+
     condition.calledWith(result).should.be.true;
     finA.calledWith(result).should.be.true;
     finB.calledWith(result).should.be.true;
+  });
+
+  describe("scrolling and focus", function () {
+    it("should scroll and focus effectively");
   });
 });
