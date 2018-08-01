c639caee773c636d72bce8bf941669d2bf39a4b7
Dan Mork <dan@humanstuff.com>
2018-07-31 21:17:09 -0500

    Resolve Issue

--- src/attaching.js
+++ src/attaching.js
@@ -1,52 +1,50 @@
 export var registrations = [];
 
 export function attach(result) {
-  if (!result) return Promise.reject(new Error("'result' param is required."));
-  if (!result.view) return Promise.reject(new Error("'result' object must have 'view' property."));
-  if (registrations.length === 0) return Promise.reject(new Error("No attachers have been registered."));
-  
-  return new Promise((resolve, reject) => {
-    var attachment = registrations.reduce((prev, registration) => {
-      if (prev) return prev;
-      
-      var attacherResult = registration.attacher(result);
-      if (!attacherResult) return;
-      
-      return {
-        registration,
-        result: attacherResult
-      };
-    }, null);
-    
-    if (!attachment) {
-      return reject(new Error("No attachment available for view."));
-    }
-    
-    if (attachment.result.discard) {
-      var err = new Error("The view was discarded by '" + attachment.registration.name + "'.");
-      err.name = "ViewDiscardedError";
-      return reject(err);
-    }
-    
-    var detachedViews = attachment.result.attach();
-    
-    if (detachedViews) {
-      detachedViews.forEach(detachedView => {
-        exports.raiseAttachDetachEvent(detachedView, "jsua-detach", false);
-        Array.from(detachedView.querySelectorAll("*")).forEach(detachedSubview => {
-          exports.raiseAttachDetachEvent(detachedSubview, "jsua-detach", false);
-        });
+  if (!result) throw new Error("'result' param is required.");
+  if (!result.view) throw new Error("'result' object must have 'view' property.");
+  if (registrations.length === 0) throw new Error("No attachers have been registered.");
+
+  var attachment = registrations.reduce((prev, registration) => {
+    if (prev) return prev;
+
+    var attacherResult = registration.attacher(result);
+    if (!attacherResult) return;
+
+    return {
+      registration,
+      result: attacherResult
+    };
+  }, null);
+
+  if (!attachment) {
+    throw new Error("No attachment available for view.");
+  }
+
+  if (attachment.result.discard) {
+    var err = new Error("The view was discarded by '" + attachment.registration.name + "'.");
+    err.name = "ViewDiscardedError";
+    throw err;
+  }
+
+  var detachedViews = attachment.result.attach();
+
+  if (detachedViews) {
+    detachedViews.forEach(detachedView => {
+      exports.raiseAttachDetachEvent(detachedView, "jsua-detach", false);
+      Array.from(detachedView.querySelectorAll("*")).forEach(detachedSubview => {
+        exports.raiseAttachDetachEvent(detachedSubview, "jsua-detach", false);
       });
-    }
-    
-    Array.from(result.view.querySelectorAll("*")).forEach(attachedSubview => {
-      exports.raiseAttachDetachEvent(attachedSubview, "jsua-attach", false);
     });
-    
-    exports.raiseAttachDetachEvent(result.view, "jsua-attach", true);
-    
-    resolve(result);
+  }
+
+  Array.from(result.view.querySelectorAll("*")).forEach(attachedSubview => {
+    exports.raiseAttachDetachEvent(attachedSubview, "jsua-attach", false);
   });
+
+  exports.raiseAttachDetachEvent(result.view, "jsua-attach", true);
+
+  return result;
 }
 
 export function raiseAttachDetachEvent(view, type, bubbles) {
@@ -58,10 +56,10 @@ export function raiseAttachDetachEvent(view, type, bubbles) {
 export function register(name, attacher) {
   if (!name) throw new Error("'name' param is required.");
   if (!attacher) throw new Error("'attacher' param is required.");
-  
+
   var newRegistration = { name, attacher };
   var oldRegistration = registrations.find(registration => registration.name === name);
-  
+
   if (oldRegistration) {
     let index = registrations.indexOf(oldRegistration);
     registrations[index] = newRegistration;
--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ function fetch(url, options) {
   options = options || {};
   var appView = findAppViewFor(options.origin);
   var urlObj = urlModule.parse(url);
-  
-  if (isSameDocumentReference(appView, urlObj, options)) {  
+
+  if (isSameDocumentReference(appView, urlObj, options)) {
     var sameDocumentReferenceView = findSameDocumentReferenceView(appView, urlObj, options);
     if (sameDocumentReferenceView) {
       if (sameDocumentReferenceView === appView) {
@@ -19,24 +19,27 @@ function fetch(url, options) {
         sameDocumentReferenceView.setAttribute("data-jsua-focus", true);
         finishing.tryToSetFocus({ view: sameDocumentReferenceView });
       }
-      
+
       return Promise.resolve({ view: sameDocumentReferenceView });
     }
-    
+
     return Promise.resolve({ view: appView });
   }
-  
+
   return Promise.resolve({ url, options })
     .then(transferring.transfer)
     .then(building.build)
-    .then(attaching.attach)
-    .then(finishing.finish);
+    .then(function (result) {
+      result = attaching.attach(result);
+      result = finishing.finish(result);
+      return result;
+    });
 }
 
 function isSameDocumentReference(appView, urlObj, options) {
   if (!appView) return false;
   if (!urlObj.hash) return false;
-  
+
   return Array.from(appView.querySelectorAll("[data-content-url]"))
     .map(el => el.getAttribute("data-content-url"))
     .some(variesByFragmentOnly(urlObj));
@@ -45,7 +48,7 @@ function isSameDocumentReference(appView, urlObj, options) {
 function variesByFragmentOnly(urlObj) {
   return function (otherUrl) {
     if (!otherUrl) return false;
-    
+
     var otherUrlObj = urlModule.parse(otherUrl);
     return otherUrlObj.protocol === urlObj.protocol &&
       otherUrlObj.host === urlObj.host &&
@@ -61,7 +64,7 @@ function findSameDocumentReferenceView(appView, urlObj, options) {
 function scrollAppViewToTop(appView) {
   var firstScrollableView = Array.from(appView.querySelectorAll("*"))
     .find(el => el.style.overflowY === "scroll");
-    
+
   if (firstScrollableView) {
     firstScrollableView.scrollTop = 0;
   }
@@ -69,13 +72,13 @@ function scrollAppViewToTop(appView) {
 
 function findAppViewFor(view) {
   if (!view) return;
-  
+
   var current = view;
-  
+
   while (current && current.matches("[data-jsua-context~=app]") === false) {
     current = current.parentElement;
   }
-  
+
   return current;
 }
 
